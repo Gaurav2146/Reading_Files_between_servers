0a4f4fbb32be619d1018aae026709700f81024f0
gaurav2146 <gaurav2146.gk@gmail.com>
2024-03-10 03:34:11 +0530

    chnages done

---
+++ Angular/fileUpload/src/app/app.component.ts
@@ -0,0 +1,104 @@
+import { HttpClient, HttpClientModule, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HttpClientModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'fileUpload';
+  percentDone:number=0;
+  count:number=0;
+  uploadChunkSize:number=1024*1024;
+  uploadStatus:any="";
+  FileId:string="HDEHBFEDBE";
+  constructor(private http: HttpClient) { }
+
+  onFileChanged(event:any) {
+    const file = event.target.files[0];
+    this.uploadFile(file);
+  }
+
+  uploadFile(file: File) {
+    this.count = 0;
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const uploadUrl = 'http://localhost:5000/upload/'+this.FileId;
+    const req = this.http.post(uploadUrl, formData, {
+      reportProgress: true,
+      observe: 'events' // This allows us to handle progress events
+    });
+
+    req.subscribe(event => {
+      this.count++;
+      if (event.type === HttpEventType.UploadProgress) {
+        if(event && event.total)
+        {
+          this.percentDone = Math.round(100 * event.loaded / (event.total));
+          console.log(`File is ${this.percentDone}% uploaded.`);
+        }
+      } else if (event instanceof HttpResponse) {
+        console.log('File uploaded successfully!', event.body);
+      }
+    }, error => {
+      console.error('Error uploading file:', error);
+    });
+  }
+
+  uploadFileInChunck(file: File)
+  {
+    let start = 0;
+
+    const uploadNextChunk = () => {
+      const chunk = file.slice(start, start + this.uploadChunkSize);
+      const formData = new FormData();
+
+      // Create a Blob from the file content without headers
+      const fileBlob = new Blob([chunk], { type: file.type });
+      formData.append('file', fileBlob);
+
+      const uploadUrl = 'http://localhost:5000/upload/'+this.FileId;
+      const req = this.http.post(uploadUrl, formData, {
+        reportProgress: true,
+        observe: 'events'
+      });
+  
+      req.subscribe(
+        event => {
+          if (event.type === HttpEventType.UploadProgress && event.total) {
+            // this.percentDone = Math.round(100 * event.loaded / event.total);
+            this.percentDone = (start/file.size)*100;
+          } else if (event instanceof HttpResponse) {
+            start += this.uploadChunkSize;
+            if (start < file.size) {
+              uploadNextChunk();
+            }
+            else
+            {
+              this.percentDone = (start/file.size)*100;
+              const uploadUrl = 'http://localhost:5000/uploadCompleted/'+this.FileId;
+              this.http.get(uploadUrl).subscribe((data)=>{
+                console.log(data);
+                this.uploadStatus = data;
+              },(error)=>{
+                this.uploadStatus = error;
+                console.log(error);
+              });
+            }
+          }
+        },
+        error => {
+          console.error('Error uploading chunk:', error);
+        }
+      );
+    };
+
+    uploadNextChunk();
+  }
+}
--- Server3/index.js
+++ Server3/index.js
@@ -1,32 +1,52 @@
 // server.js
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 const fs = require('fs');
 
-app.post('/upload', (req, res) => {
+
+app.use(cors({
+  origin: '*', // Change this to your allowed origins, e.g., 'http://example.com'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Change this to your allowed methods
+}));
+
+app.post('/upload/:FileId', async (req, res) => {
+
   if (!req.headers['content-type'] || !req.headers['content-type'].includes('multipart/form-data')) {
     return res.status(400).send('Bad Request: Content-Type must be multipart/form-data');
   }
 
-  let writeStream = fs.createWriteStream('uploaded_file.txt');
+  const writeStream = fs.createWriteStream("uploaded_file.txt", { flags: 'a' });
 
   req.on('data', (chunk) => {
+    console.log(chunk.length, "chunk");
     writeStream.write(chunk);
   });
 
   req.on('end', () => {
     writeStream.end();
-    res.send('File uploaded successfully');
+    res.status(200).json({ message: 'File uploaded successfully' });
   });
 
   req.on('error', (err) => {
     console.error('Error uploading file:', err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ message: 'Internal Server Error' });
   });
 });
+
+
+app.get("/uploadCompleted/:FileId", (req, res, next) => {
+  try {
+    let { FileId } = req.params;
+    console.log("FileId " + FileId + " is Uploaded Successfully");
+    res.status(200).json({ success: true })
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
